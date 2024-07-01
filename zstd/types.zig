fd5555fc8ba394cde4bbb00d147b2b9a4a0b72ea
sOkam <7308253+heysokam@users.noreply.github.com>
2024-07-01 09:35:52 +0200

    new: `Name` and `Version` types

--- zstd/types.zig
+++ zstd/types.zig
@@ -6,6 +6,7 @@
 // @deps std
 const std = @import("std");
 
+
 //______________________________________
 // @section Array Aliases
 //____________________________
@@ -30,4 +31,25 @@ pub const Str = ByteBuffer;
 //____________________________
 /// @descr Dummy type. Marks that a field is not used yet
 pub const todo = ?u8;
+
+
+//______________________________________
+// @section Name
+//____________________________
+/// @descr Describes a General Purpose Name.
+pub const Name = struct {
+  /// @descr Short version of the name. Mandatory.
+  short  :cstr,
+  /// @descr Long version of the name. Will use {@link Name.short} when not specified.
+  long   :?cstr= null,
+  /// @descr Human-readable version of the name. Will use {@link Name.long} when not specified.
+  human  :?cstr= null,
+};
+
+
+//______________________________________
+// @section Other Aliases
+//____________________________
+/// @descr Describes a software version, using the Semantic Versioning 2.0.0 specification.
+pub const Version = std.SemanticVersion;
 
