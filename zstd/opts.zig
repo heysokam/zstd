587196b41cde6269c7f6817c26154ae1d8c271bb
sOkam <7308253+heysokam@users.noreply.github.com>
2024-07-15 04:27:14 +0200

    doc: Add zstd.opts reference implementation

---
+++ zstd/opts.zig
@@ -0,0 +1,8 @@
+//_____________________________________________________________
+// @reference
+// var args = try std.process.argsWithAllocator(A.allocator());
+// defer args.deinit();
+// while (args.next()) | arg | {
+//   prnt("{s}\n", .{arg});
+// }
+//_____________________________________________________________
