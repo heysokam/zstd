aca9d68a67015245eeaf25cf7509c88dd3f1bdba
sOkam <7308253+heysokam@users.noreply.github.com>
2025-08-28 09:17:17 +0200

    new: Optional `log.prnt` helper for formatted logging

--- src/zstd/log.zig
+++ src/zstd/log.zig
@@ -9,18 +9,21 @@ const std = @import("std");
 //______________________________________
 // @section Aliased exports from std
 //____________________________
-pub const prnt = @import("std").debug.print;
+pub const dbg  = @import("std").debug.print;
 pub const fail = @import("std").debug.panic;
 
 
 //______________________________________
 /// @descr Outputs the {@arg msg} to CLI with an added \n at the end.
-pub fn echo (msg :[]const u8) void {
+pub fn prnt (comptime fmt: []const u8, msg: anytype) void {
   // stdout for the output of the app, not for debugging messages.
   var stdout_buffer :[1024]u8= undefined;
   var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
   const stdout = &stdout_writer.interface;
-  stdout.print("{s}\n", .{msg}) catch unreachable;
+  stdout.print(fmt++"\n", msg) catch unreachable;
   stdout.flush() catch unreachable;
 }
+//______________________________________
+/// @descr Outputs the {@arg msg} to CLI with an added \n at the end.
+pub fn echo (msg :[]const u8) void { prnt("{s}", .{msg}); }
 
